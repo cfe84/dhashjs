a5e630b83cb1308b3863392944af214c4af1bfb8
Charles Feval <c@feval.ca>
2022-05-26 22:25:28 -0700

    Add memory extension

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Calculates a d-hash for pictures.
 Usage:
 
 ```js
-import { dhash } from "dhashjs";
+import { dHash } from "dhashjs";
 import * as fs from "fs";
 
 const content = fs.readFileSync("picture.jpg");
-dhash.calculateHashAsync(content).then(hash => console.log(hash));
+dHash.calculateHashAsync(content).then(hash => console.log(hash));
 ```
--- src/DHash.ts
+++ src/DHash.ts
@@ -1,26 +1,44 @@
 import * as Jimp from "jimp"
+import * as JPEG from "jpeg-js"
 
 export class DHash {
-  static async calculateHashAsync(picture: Buffer): Promise<string> {
-    const res: Uint8Array = new Uint8Array(8)
-    const image = await Jimp.read(picture)
-    const resized = await image.resize(9, 8)
-    const gray = await resized.grayscale()
-    for (let y = 0; y < 8; y++) {
-      let rowRes = 0
-      let pix1 = await gray.getPixelColor(0, y)
-      for (let x = 1; x < 9; x++) {
-        rowRes = rowRes << 1
-        let pix2 = await gray.getPixelColor(x, y)
-        const diff = pix1 - pix2
-        if (diff >= 0) {
-          rowRes += 1
-        }
-        pix1 = pix2
-      }
-      res[y] = rowRes
+  static memoryExtensionInMb: 1024
+  static memoryExtensionRequired = true
+
+  private static extendMemory() {
+    if (this.memoryExtensionRequired) {
+      Jimp.decoders['image/jpeg'] = (data: Buffer) => JPEG.decode(data, { maxMemoryUsageInMB: DHash.memoryExtensionInMb })
+      this.memoryExtensionRequired = false
     }
-    const buffer = Buffer.from(res)
-    return buffer.toString("base64")
+  }
+
+  static async calculateHashAsync(picture: Buffer): Promise<string> {
+    try {
+
+      this.extendMemory()
+      const res: Uint8Array = new Uint8Array(8)
+      const image = await Jimp.read(picture)
+      const resized = await image.resize(9, 8)
+      const gray = await resized.grayscale()
+      for (let y = 0; y < 8; y++) {
+        let rowRes = 0
+        let pix1 = await gray.getPixelColor(0, y)
+        for (let x = 1; x < 9; x++) {
+          rowRes = rowRes << 1
+          let pix2 = await gray.getPixelColor(x, y)
+          const diff = pix1 - pix2
+          if (diff >= 0) {
+            rowRes += 1
+          }
+          pix1 = pix2
+        }
+        res[y] = rowRes
+      }
+      const buffer = Buffer.from(res)
+      return buffer.toString("base64")
+
+    } catch (err) {
+      throw err
+    }
   }
 }
